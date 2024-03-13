690438067de101648f6386e29f418315a52fc560
Ashish <ashishrajle700@gmail.com>
2024-03-13 12:48:21 +0530

    testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ imp;
 const App = () => {
   return (
     <div>
-      <h1 className="bg-slate-600 text-white">app</h1>
+      <h1 className="bg-slate-600 text-white">app testing</h1>
     </div>
   );
 };
