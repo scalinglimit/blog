86d3ed631aa08097d566cd9706512cda65f25b44
Ashish rajle <ashdhs50@gmail.com>
2024-05-03 19:00:36 +0530

    prebuild

---
+++ _ReadMe first.txt
@@ -0,0 +1,9 @@
+project explanation = A Blog website, with authentication using jwt and oAuth, 
+                      Admin users have access to post creation page and dashboard page.
+                      Admin information is available on the signin page.
+               
+
+How to Run  
+
+1] run backend = npm run dev, in the root folder.
+2] run client = npm run dev, in the client folder.
--- api/index.js
+++ api/index.js
@@ -5,12 +5,20 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log("connected to mongodb");
-});
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("connected to mongodb");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -18,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000");
 });
 
 //routes
@@ -26,10 +34,16 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 
+app.use(express.static(path.join(__dirname, "/client/dist"))); //for deployment
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 //error middleware
 app.use((error, req, res, next) => {
-  console.log(error);
-  const statusCode = error.statusCode || 500;
-  const message = error.message || "internal server error";
-  res.status(statusCode).json({ success: false, statusCode, message });
+    console.log(error);
+    const statusCode = error.statusCode || 500;
+    const message = error.message || "internal server error";
+    res.status(statusCode).json({ success: false, statusCode, message });
 });
